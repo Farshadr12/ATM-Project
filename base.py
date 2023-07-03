9094fe37efd7a8ced222f68c16257fad986ca5ec
farshadr12 <farshadr12@gmail.com>
2023-07-03 03:04:19 +0200

    modified:   base.py

--- base.py
+++ base.py
@@ -11,6 +11,11 @@ class User:
 
     def DepositAmount(self):
         amount = int(input("Please specify the amount to deposit >> "))
+        self.wallet += amount
+        print(f"your new ballance is {self.wallet}")
+
+    def WithdrawAmount(self):
+        amount = int(input("Please specify the amount to Withdraw >> "))
         self.wallet -= amount
         print(f"your new ballance is {self.wallet}")
 
@@ -19,6 +24,9 @@ user001 = User("user1", 1234, 500)
 user002 = User("user2", 0000, 5000)
 user_list = [user001, user002]
 user_found = False
+user_auth = False
+# Var in menu
+selected_item = 0
 
 while True:
     current_user = input("Please enter your username >> ")
@@ -28,10 +36,37 @@ while True:
             current_user = i
             break
     if user_found:
-        break
+        for x in range(3):
+            entered_password = int(input("Please Enter Your Password >> "))
+            if entered_password == current_user.pw:
+                print("Login Successfull")
+                user_auth = True
+                break
+        if user_auth:
+            break
+        else:
+            print("Login Failed, Try Again!")
+            pass
     else:
         print("User Not Found !!")
         continue
 
 print("Found !!!")
-current_user.DepositAmount()
+while True:
+    print("Please Select one Option from bellow:")
+    print("""1 to See Wallet
+2 to Deposit Money
+3 to Withdraw Money
+9 to Exit""")
+    selected_item = int(input('>>> '))
+    if selected_item == 9:
+        print('GoodBye !')
+        break
+    elif selected_item == 2:
+        current_user.DepositAmount()
+    elif selected_item == 1:
+        print('your Ballance is ' + str(current_user.wallet))
+    elif selected_item == 3:
+        current_user.WithdrawAmount()
+    else:
+        print('Make sure you enter item number from the menue')
